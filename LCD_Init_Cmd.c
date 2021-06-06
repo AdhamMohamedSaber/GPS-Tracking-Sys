f2e90e6f173590f73e482bc7c0b2b064069b1908
AhmedAssy <85446991+AhmedAssy@users.noreply.github.com>
2021-06-06 19:29:14 +0200

    Add files via upload
    LCD Command Fn & Initialization Fn

---
+++ LCD_Init_Cmd.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "tm4c123gh6pm.h"
+#include <math.h>
+#include <stdint.h>
+void Lcd_command(char com)
+{
+        GPIO_PORTA_DATA_R &=0x1F;
+        GPIO_PORTA_DATA_R |=0x80;
+        GPIO_PORTB_DATA_R =com;
+        delay_milli(1);
+}
+
+void PORTA_INIT(void)
+{
+    SYSCTL_RCGCGPIO_R|=0x03;
+    while ((SYSCTL_PRGPIO_R&0x03 )==0);
+
+	//  LCD_COMMAND
+    GPIO_PORTA_DIR_R=0xE0;
+    GPIO_PORTA_DATA_R &=0x1F;
+    GPIO_PORTA_AMSEL_R=0;
+    GPIO_PORTA_CR_R |=0xE0;
+    GPIO_PORTA_AFSEL_R=0;
+    GPIO_PORTA_PUR_R=0x00;
+    GPIO_PORTA_DEN_R=0xE0;
+
+// LCD_COMMAND_init
+    Lcd_command(0x30);
+    delay_micro(40);
+    Lcd_command(0x01);
+    delay_micro(1640);
+    Lcd_command(0x80);
+    delay_micro(40);
+    Lcd_command(0x0F);
+    delay_micro(40);
+    Lcd_command(0x06);
+    delay_micro(40);
+    Lcd_command(0x38);
+    delay_micro(40);
+}

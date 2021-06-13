e221d9e47f097594dc962937f0b5b91ed318cd00
MahmoudAAbdelkader <61502610+MahmoudAAbdelkader@users.noreply.github.com>
2021-06-13 17:04:04 +0200

    Create LCD_DATA.h

---
+++ LCD_DATA.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "tm4c123gh6pm.h"
+#include <math.h>
+#include <stdint.h>
+#define no_of_points 2
+#define M_PI 3.14159265358979323846
+#define rad 6371
+#include<string.h>
+
+//  LCD_DATA
+void Lcd_Data(char data){
+
+}
+
+void PORTB_INIT (void){
+
+}

71f7955c60e27b67fa88896562008aef7f176070
AhmedAssy <85446991+AhmedAssy@users.noreply.github.com>
2021-06-13 05:33:15 +0200

    Update LCD_Init_Cmd.c

--- LCD_Init_Cmd.c
+++ LCD_Init_Cmd.c
@@ -1,7 +1,16 @@
-#include <stdio.h>
-#include "tm4c123gh6pm.h"
-#include <math.h>
-#include <stdint.h>
+#ifndef STDIO_H
+#define STDIO_H
+#endif
+#ifndef TM4C123GH6PM_H
+#define TM4C123GH6PM_H
+#endif
+#ifndef STDINT_H
+#define STDINT_H
+#endif
+#ifndef MATH_H
+#define MATH_H
+#endif
+
 void Lcd_command(char com)
 {
         GPIO_PORTA_DATA_R &=0x1F;
@@ -24,7 +33,7 @@ void PORTA_INIT(void)
     GPIO_PORTA_PUR_R=0x00;
     GPIO_PORTA_DEN_R=0xE0;
 
-// LCD_COMMAND_init
+	// LCD_COMMAND_init
     Lcd_command(0x30);
     delay_micro(40);
     Lcd_command(0x01);

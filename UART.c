df72a450924026e77e24384b26fa86e83b4fdfcc
AhmedAssy <85446991+AhmedAssy@users.noreply.github.com>
2021-06-13 23:54:35 +0200

    Update UART.c

--- UART.c
+++ UART.c
@@ -7,30 +7,39 @@
 #define rad 6371
 #include<string.h>
 
-void UART0_INT(void){  // WORKING ON PORT A
-	SYSCTL_RCGCUART_R |= 0x01;
-	SYSCTL_RCGCGPIO_R |= 0x01;
+void UART2_Init(void)
+	{
+    SYSCTL_RCGCUART_R |= 0x04;
+    while((SYSCTL_PRUART_R & 0x04) == 0);
+    SYSCTL_RCGCGPIO_R |= 0x08;
+		while((SYSCTL_PRGPIO_R & 0x08) == 0);
 	
-	UART0_CTL_R &= ~0x01;
-	// LET THE BR IS 9600/16MHZ
-	UART0_IBRD_R = 104;
-	UART0_FBRD_R = 11;
-	UART0_LCRH_R = 0x70;
-	//(UART_LCRH_WLEN_8 | UART_LCRH_FEN);
-	UART0_CTL_R |= 0x301;
-	
-	GPIO_PORTA_AFSEL_R |= 0x03;   
-	GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R & 0xFFFFFF00) + 0x00000011;
-	GPIO_PORTA_DEN_R |= 0x03;
-	GPIO_PORTA_AMSEL_R &=~ 0x03;
-}
+	  GPIO_PORTD_LOCK_R = 0x4C4F434B;
+	  GPIO_PORTD_CR_R |= 0xC0;
+    GPIO_PORTD_AMSEL_R &= ~0xC0;
+    GPIO_PORTD_AFSEL_R |= 0xC0;
+    GPIO_PORTD_PCTL_R = (GPIO_PORTD_PCTL_R & 0x00FFFFFF) + 0x11000000;
+    GPIO_PORTD_DEN_R |= 0xC0;
+    
+    UART2_CTL_R &= ~0x00000001;
+    UART2_IBRD_R = 104;
+    UART2_FBRD_R = 11;
+	  UART2_LCRH_R = 0x00000070;
+    UART2_CTL_R |= 0x00000301;
+	}
 
-uint8_t UART0_READING (void){
-	while ((UART0_FR_R & 0x10) != 0);   // WAIT UNTIL RXFE BE 0
-	return (uint8_t)(UART0_DR_R & 0xFF); 
-}
+char UART2_InChar(void)
+	{
+     while ((UART2_FR_R & 0x00000010) != 0);
+		 return ((char) (UART2_DR_R & 0xFF));
+	}
 
-void UART0_WRITING (uint8_t data){
-	while ((UART0_FR_R & 0x20) != 0);  // WAIT UNTIL TXFF BE 0
-	UART0_DR_R = data;
+void UART2_GetData(char *data, int len)
+	{
+     char character;
+	   int i;
+	   for (i = 0; 1 < len; i++) {
+		     character = UART2_InChar();
+			   data[i] = character;
+		 }
 }

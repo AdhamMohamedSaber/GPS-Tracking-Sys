4a7bfb5b9917c7a6a377973d413649bb8be977f2
MohamedHosamENG <85447359+MohamedHosamENG@users.noreply.github.com>
2021-06-14 00:02:01 +0200

    Update Main, fn of distance calculations.c

--- Main, fn of distance calculations.c
+++ Main, fn of distance calculations.c
@@ -8,93 +8,118 @@
 #include<string.h>
 //#include <cmath>
 
-//uint16_t total_Distance;
-//uint32_t result;
+void UART2_Init(void)
+	{
+    SYSCTL_RCGCUART_R |= 0x04;
+    while((SYSCTL_PRUART_R & 0x04) == 0);
+    SYSCTL_RCGCGPIO_R |= 0x08;
+		while((SYSCTL_PRGPIO_R & 0x08) == 0);
+	
+	  GPIO_PORTD_LOCK_R = 0x4C4F434B;
+	  GPIO_PORTD_CR_R |= 0xC0;
+    GPIO_PORTD_AMSEL_R &= ~0xC0;
+    GPIO_PORTD_AFSEL_R |= 0xC0;
+    GPIO_PORTD_PCTL_R = (GPIO_PORTD_PCTL_R & 0x00FFFFFF) + 0x11000000;
+    GPIO_PORTD_DEN_R |= 0xC0;
+    
+    UART2_CTL_R &= ~0x00000001;
+    UART2_IBRD_R = 104;
+    UART2_FBRD_R = 11;
+	  UART2_LCRH_R = 0x00000070;
+    UART2_CTL_R |= 0x00000301;
+	}
 
-//unsigned char LCD_word[]={'M','A','X','E','R','I','E','N','C','E'};
+char UART2_InChar(void)
+	{
+     while ((UART2_FR_R & 0x00000010) != 0);
+		 return ((char) (UART2_DR_R & 0xFF));
+	}
 
+void UART2_GetData(char *data, int len)
+	{
+     char character;
+	   int i;
+	   for (i = 0; 1 < len; i++) {
+		     character = UART2_InChar();
+			   data[i] = character;
+		 }
+	}
 	 
 void SystemInit(void);
 
 /* The Main includes the calling of all the functions plus the printing on the LCD. 
 Also including part of distance calculations and the function of turning the LED if the distance was greater than or equal 100.*/
 
-int main (void){
-		double prev_lat = 0;
-    double prev_lon = 0;
-    double lon[ ] = {31.278169737933965, 31.28100215080061};
-    double lat[ ] = {30.062523380301965, 30.063484433684938};
-    double distance = 0;
-		char lcd_chars[50];
-		int i =0 ;
+int main (void)
+{
+	
+	double prev_lat = 0;
+  double prev_lon = 0;
+	double lon = 0;
+	double lat = 0;
+  double distance = 112;
+  char  buf[3];
+	int distancex = 0;
+	int i =0 ;
+	
+  UART2_Init();
+	LCD_INIT();
+
+	
+	
+	
+ while(1) {
+				// LCD reading
+
+			UART2_GetData(data, len); //comment
+				delay_milli(10);
+		  gps_data (data, &lat, &lon);
+      if (prev_lat != 0 && prev_lon != 0)
+				{
+					distance += haversine(prev_lat, prev_lon, lat, lon);
+				}
+			if (distance >= 100) PORTF_Int(); //Light Red LED ON
+
+      prev_lat = lat;
+      prev_lon = lon;
+
+      
+      distancex = (int) distance ;
+			snprintf(buf, 4, "%d", distancex);
+			
+				Lcd_command (0x01); //Clear display
+			
+				Lcd_Data(' ');
+				delay_milli(1);
+				Lcd_Data('D');
+				delay_milli(1);
+				Lcd_Data('I');
+				delay_milli(1);
+				Lcd_Data('S');
+				delay_milli(1);
+				Lcd_Data('T');
+				delay_milli(1);
+				Lcd_Data('.');
+				delay_milli(5);
+				delay_milli(1);
+				Lcd_Data(' ');
+				delay_milli(1);
+				Lcd_Data('=');
+				delay_milli(1);
+			
 		
-    PORTF_Int();
-		PORTA_INIT();
-		PORTB_INIT();
-		
-		UART0_INT();
-		//UART0_WRITING();
-		UART0_READING();            // THAT WILL BE DETERMINE ACCORDING TO THE NEXT PHASE 
-    
-		
-		//sendString();
-		
-		// Distance code
-		 
-		sprintf(lcd_chars,"%d", (int) distance);
-		
-    for(i=0; i<no_of_points; i++){
-        if (prev_lat != 0 && prev_lon != 0){
-            distance += haversine(prev_lat, prev_lon, lat[i], lon[i]);
-            }
-				
-				if (distance >= 100)
-            GPIO_PORTF_DATA_R = 0X20;
-        
-        prev_lat = lat[i];
-        prev_lon = lon[i];
-        
-        printf("%f m. \n", distance);
-    }
-		
-		// LCD reading
-		
-		Lcd_Data('T');
-		delay_milli(1);
-		Lcd_Data('O');
-		delay_milli(1);
-		Lcd_Data('T');
-		delay_milli(1);
-		Lcd_Data('A');
-		delay_milli(1);
-		Lcd_Data('L');
-		delay_milli(1);
-		Lcd_Data(' ');
-		delay_milli(1);
-		Lcd_Data('D');
-		delay_milli(1);
-		Lcd_Data('I');
-		delay_milli(1);
-		Lcd_Data('S');
-		delay_milli(1);
-		Lcd_Data('T');
-		delay_milli(1);
-		Lcd_Data('.');
-		delay_milli(5);
-		delay_milli(1);
-		Lcd_Data(' ');
-		delay_milli(1);
-		Lcd_Data('=');
-		delay_milli(1);
-		Lcd_Data(lcd_chars[0]);
-		delay_milli(1);
-		Lcd_Data(lcd_chars[1]);
-		delay_milli(1);
-		Lcd_Data(lcd_chars[2]);
-		delay_milli(1);
-		Lcd_Data(' ');
-		
-		delay_milli(500);
-		
-		return 0;
+			Lcd_Data(buf[0]);
+			delay_milli(1);
+			Lcd_Data(buf[1]);
+			delay_milli(1);
+			Lcd_Data(buf[2]);
+			delay_milli(1);
+			Lcd_Data('m');
+			delay_milli(1);
+			Lcd_Data(' ');
+			delay_milli(1);
+			delay_milli(500);
+		}
+	
 }
+

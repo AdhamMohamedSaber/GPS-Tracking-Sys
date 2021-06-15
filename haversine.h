fc585adcc7b9e58dadfbb5bed2bf90767a1c4ba2
MahmoudAAbdelkader <61502610+MahmoudAAbdelkader@users.noreply.github.com>
2021-06-15 18:58:21 +0200

    Create haversine.h

---
+++ haversine.h
@@ -0,0 +1,7 @@
+double deg2rad(double);
+double rad2deg(double);
+
+double haversine (double lat1, double lon1, double lat2, double lon2, char unit);
+double deg2rad(double deg);
+
+double rad2deg(double radi);

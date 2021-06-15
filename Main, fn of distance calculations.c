0be5f7e6a48b9d301c5533e10e9d8185beb0a28a
MohamedHosamENG <85447359+MohamedHosamENG@users.noreply.github.com>
2021-06-15 18:55:41 +0200

    Update Main, fn of distance calculations.c

--- Main, fn of distance calculations.c
+++ Main, fn of distance calculations.c
@@ -1,84 +1,37 @@
-#include <stdio.h>
-#include "tm4c123gh6pm.h"
-#include <math.h>
-#include <stdint.h>
-#define no_of_points 2
-#define M_PI 3.14159265358979323846
-#define rad 6371
-#include<string.h>
-//#include <cmath>
-
-void UART2_Init(void)
-	{
-    SYSCTL_RCGCUART_R |= 0x04;
-    while((SYSCTL_PRUART_R & 0x04) == 0);
-    SYSCTL_RCGCGPIO_R |= 0x08;
-		while((SYSCTL_PRGPIO_R & 0x08) == 0);
+double lon = 0;
+double lat = 0;
+volatile double prev_lat = 0;
+  volatile double prev_lon = 0;
 	
-	  GPIO_PORTD_LOCK_R = 0x4C4F434B;
-	  GPIO_PORTD_CR_R |= 0xC0;
-    GPIO_PORTD_AMSEL_R &= ~0xC0;
-    GPIO_PORTD_AFSEL_R |= 0xC0;
-    GPIO_PORTD_PCTL_R = (GPIO_PORTD_PCTL_R & 0x00FFFFFF) + 0x11000000;
-    GPIO_PORTD_DEN_R |= 0xC0;
-    
-    UART2_CTL_R &= ~0x00000001;
-    UART2_IBRD_R = 104;
-    UART2_FBRD_R = 11;
-	  UART2_LCRH_R = 0x00000070;
-    UART2_CTL_R |= 0x00000301;
-	}
-
-char UART2_InChar(void)
-	{
-     while ((UART2_FR_R & 0x00000010) != 0);
-		 return ((char) (UART2_DR_R & 0xFF));
-	}
-
-void UART2_GetData(char *data, int len)
-	{
-     char character;
-	   int i;
-	   for (i = 0; 1 < len; i++) {
-		     character = UART2_InChar();
-			   data[i] = character;
-		 }
-	}
-	 
-void SystemInit(void);
-
-/* The Main includes the calling of all the functions plus the printing on the LCD. 
-Also including part of distance calculations and the function of turning the LED if the distance was greater than or equal 100.*/
+  volatile double distance = 0;
 
 int main (void)
 {
-	
-	double prev_lat = 0;
-  double prev_lon = 0;
-	double lon = 0;
-	double lat = 0;
-  double distance = 112;
+	bool test_me = true;
   char  buf[3];
+	int counter;
 	int distancex = 0;
-	int i =0 ;
+	volatile	int i =0 ;
 	
-  UART2_Init();
+  UART5_Init();
 	LCD_INIT();
+	
+
+	delay_milli(4000);
 
 	
+while(1) {
 	
+ test_me = check_me_again();
 	
- while(1) {
-				// LCD reading
-
-			UART2_GetData(data, len); //comment
-				delay_milli(10);
-		  gps_data (data, &lat, &lon);
+if (test_me == true)
+{
+		  gps_data2 (&lat, &lon);
       if (prev_lat != 0 && prev_lon != 0)
 				{
-					distance += haversine(prev_lat, prev_lon, lat, lon);
+					distance += haversine(prev_lat, prev_lon, lat, lon,'K');
 				}
-			if (distance >= 100) PORTF_Int(); //Light Red LED ON
+			if (distance >= 100) PORTF_Int(); //Light Blue LED ON
 
       prev_lat = lat;
       prev_lon = lon;
@@ -88,7 +41,6 @@ int main (void)
 			snprintf(buf, 4, "%d", distancex);
 			
 				Lcd_command (0x01); //Clear display
-			
 				Lcd_Data(' ');
 				delay_milli(1);
 				Lcd_Data('D');
@@ -107,19 +59,19 @@ int main (void)
 				Lcd_Data('=');
 				delay_milli(1);
 			
-		
-			Lcd_Data(buf[0]);
+	for(counter= 0; counter < 3; counter++){		
+			Lcd_Data(buf[counter]);
 			delay_milli(1);
-			Lcd_Data(buf[1]);
-			delay_milli(1);
-			Lcd_Data(buf[2]);
-			delay_milli(1);
+	}
 			Lcd_Data('m');
 			delay_milli(1);
-			Lcd_Data(' ');
+			Lcd_Data('.');
+			delay_milli(1);
+			Lcd_Data('.');
 			delay_milli(1);
 			delay_milli(500);
-		}
-	
+	}
+else
+		continue;	
 }
-
+}
